d4b33fc6f30353d9c4fa480f74dae318c06cbd6c
Gabriela Salvo <gabrielamdesalvo@gmail.com>
2020-09-23 13:29:44 -0300

    get all repositories, create new repository, like repository

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 // const { v4: uuid } = require('uuid');
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +12,40 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
+
+/********** POST**********/
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.put("/repositories/:id", (request, response) => {});
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository =>repository.id ===id);
+  repository.likes +=1;
+
+  return response.json(repository)
 });
 
 module.exports = app;
